de595d2b948d1a2b727b674cdf964186fd2d6d87
muskenum <peter.varga96@gmail.com>
2020-05-06 18:02:57 +0200

    Fixed dependencies

--- platform/middlewares/auth/checkUserCredentialsMW.js
+++ platform/middlewares/auth/checkUserCredentialsMW.js
@@ -2,18 +2,18 @@ const requireOption = require('../requireOption');
 
 module.exports = function (objectRepository) {
     return function (req, res, next) {
-        /*if ((typeof req.body.password === 'undefined') &&
+        if ((typeof req.body.password === 'undefined') ||
             (typeof req.body.username === 'undefined')) {
             return next();
-        } else if ((req.body.password === 'alma') && (req.body.username === 'peti')) {
+        } else if ((req.body.password === 'alma') && (req.body.username === 'admin')) {
             req.session.loggedin = true;
-            req.session.userID = 33;
+            req.session.userID = 69;
             return req.session.save((err) => {
                 return res.redirect('/games');
             });
         } else {
             res.locals.error = 'Wrong username or password!'
             return next();
-        }*/next();
+        }next();
     };
 };
