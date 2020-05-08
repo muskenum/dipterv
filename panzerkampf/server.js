3282f27df8b0fe5335e37708ae0c01e6189a7897
muskenum <peter.varga96@gmail.com>
2020-05-09 00:01:45 +0200

    Started making the framework for the game

--- panzerkampf/server.js
+++ panzerkampf/server.js
@@ -1,10 +1,11 @@
-var express = require('express');
-var http = require('http');
-var path = require('path');
-var socketIO = require('socket.io');
-var app = express();
-var server = http.Server(app);
-var io = socketIO(server);
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const socketIO = require('socket.io');
+
+const app = express();
+const server = http.Server(app);
+const io = socketIO(server);
 
 app.set('port', 5000);
 
@@ -14,10 +15,35 @@ app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-server.listen(5000, function () {
+server.listen(5000, () => {
     console.log('Listening on port 5000');
 });
 
-io.on('connection', function (socket) {
-    io.sockets.emit('message', 'A user has connected.');
-}, 1000);
+let players = {};
+io.on('connection', socket => {
+    socket.on('new player', () => {
+        players[socket.id] = {
+            x: 300,
+            y: 300
+        };
+    });
+    socket.on('movement', data => {
+        let player = players[socket.id] || {};
+        if (data.up) {
+            player.y -= 5;
+        }
+        if (data.left) {
+            player.x -= 5;
+        }
+        if (data.down) {
+            player.y += 5;
+        }
+        if (data.right) {
+            player.x += 5;
+        }
+    });
+});
+
+setInterval(() => {
+    io.sockets.emit('state', players);
+}, 1000 / 60);
